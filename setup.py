009faf21025acc402a73d85fe97c0425ab421b91
erdogant <erdogant@gmail.com>
2019-12-28 21:14:11 +0100

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
      install_requires=['pandas','numpy','seaborn','networkx>2'],
      python_requires='>=3.6',
      name='d3graph',  
-     version='0.1.0',
+     version='0.1.1',
 #     version=versioneer.get_version(),    # VERSION CONTROL
 #     cmdclass=versioneer.get_cmdclass(),  # VERSION CONTROL
      author="Erdogan Taskesen",
@@ -16,7 +16,7 @@ setuptools.setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/erdoganta/d3graph",
-	 download_url = 'https://github.com/erdoganta/d3graph/archive/0.1.0.tar.gz',
+	 download_url = 'https://github.com/erdoganta/d3graph/archive/0.1.1.tar.gz',
      packages=setuptools.find_packages(), # Searches throughout all dirs for files to include
      include_package_data=True, # Must be true to include files depicted in MANIFEST.in
      classifiers=[
