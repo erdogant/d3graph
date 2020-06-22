2901311f8c5ce81cfc210dbe0d543ac246a42d70
Erdogan Taskesen <erdogant@gmail.com>
2020-06-22 09:53:06 +0200

    new cleaning files added

---
+++ make_clean.sh
@@ -0,0 +1,9 @@
+echo "Cleaning previous builds first.."
+rm -rf dist
+rm -rf build
+rm -rf d3graph.egg-info
+rm -rf d3graph/__pycache__
+rm -rf d3graph/d3js
+rm -rf __pycache__
+rm -rf .pytest_cache
+rm -rf .pylint.d
