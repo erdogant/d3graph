7c976deba2097053892699e9ccc7cddb624d9c6b
Erdogan Taskesen <erdogant@gmail.com>
2020-06-22 19:03:55 +0200

    correct version

--- d3graph/__init__.py
+++ d3graph/__init__.py
@@ -7,7 +7,7 @@ from d3graph.utils.adjmat_vec import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.5'
 
 # module level doc-string
 __doc__ = """
