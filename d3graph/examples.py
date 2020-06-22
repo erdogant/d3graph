a4b7b201a9ac8e41e0af302603e385fd48a8ea5e
Erdogan Taskesen <erdogant@gmail.com>
2020-06-22 09:33:00 +0200

    replaces by examples.py

--- d3graph/examples.py
+++ d3graph/examples.py
@@ -10,6 +10,7 @@ G = nx.karate_club_graph()
 adjmat = nx.adjacency_matrix(G).todense()
 adjmat=pd.DataFrame(index=range(0,adjmat.shape[0]), data=adjmat, columns=range(0,adjmat.shape[0]))
 
+out = d3graph(adjmat)
 out = d3graph(adjmat, savepath='c://temp/')
 out = d3graph(adjmat, node_color=adjmat.index.values, savepath='c://temp/')
 out = d3graph(adjmat, node_color=adjmat.index.values, node_color_edge='#fffff',savepath='c://temp/')
@@ -37,8 +38,8 @@ for i in range(0,len(G.nodes)):
 
 out = d3graph(adjmat, df=df, node_size=node_size)
 out = d3graph(adjmat, df=df, node_color=node_size, node_size=node_size)
-out = d3graph(adjmat, df=df, node_color=node_size, node_size=node_size, edge_distance=100)
-out = d3graph(adjmat, df=df, node_color=node_size, node_size=node_size, charge=50)
+out = d3graph(adjmat, df=df, node_color=node_size, node_size=node_size, edge_distance=1000)
+out = d3graph(adjmat, df=df, node_color=node_size, node_size=node_size, charge=1000)
 out = d3graph(adjmat, df=df, node_color=node_name, node_size=node_size, node_size_edge=node_size, node_color_edge='#00FFFF', cmap='Set1', collision=1, charge=250)
 
 # %%
