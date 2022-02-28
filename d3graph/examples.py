0daf1f98a2b85d34850128eeb00383178e5da773
erdogant <erdogant@gmail.com>
2022-03-01 00:50:17 +0100

    examples improvements

--- d3graph/examples.py
+++ d3graph/examples.py
@@ -5,82 +5,51 @@ import numpy as np
 from d3graph import d3graph, vec2adjmat
 
 # %%
-from d3graph import d3graph, vec2adjmat
+from d3graph import d3graph
 
-source = ['node A','node F','node B','node B','node B','node A','node C','node Z']
-target = ['node F','node B','node J','node F','node F','node M','node M','node A']
-weight = [5.56, 0.5, 0.64, 0.23, 0.9, 3.28, 0.5, 0.45]
-
-adjmat = vec2adjmat(source, target, weight=weight)
 d3 = d3graph()
+# Load example
+adjmat = d3.import_example('small')
+
 d3.graph(adjmat)
 d3.show()
 
 d3.set_node_properties(color=adjmat.columns.values, label=['node AA','node BB','node FF','node JJ','node MM','node CC','node ZZ'])
-d3.show(filepath='c://temp/')
+d3.show()
 
 
 
 # %%
-G = nx.karate_club_graph()
-adjmat = nx.adjacency_matrix(G).todense()
-adjmat=pd.DataFrame(index=range(0,adjmat.shape[0]), data=adjmat, columns=range(0,adjmat.shape[0]))
-adjmat.columns=adjmat.columns.astype(str)
-adjmat.index=adjmat.index.astype(str)
-adjmat.iloc[3,4]=5
-adjmat.iloc[4,5]=6
-adjmat.iloc[5,6]=7
 
-df = pd.DataFrame(index=adjmat.index)
-df['degree']=np.array([*G.degree()])[:,1]
-df['other info']=np.array([*G.degree()])[:,1]
-node_size=df.degree.values*2
-node_color=[]
-
-for i in range(0,len(G.nodes)):
-    node_color.append(G.nodes[i]['club'])
-    node_name=node_color
-df['name']=node_name
 
 # Make some graphs
 d3 = d3graph()
+# Load example
+adjmat, df = d3.import_example('karate')
 d3.graph(adjmat)
-d3.set_node_properties(color=node_color, cmap='Set1')
+d3.set_node_properties(color=df['label'].values, cmap='Set1')
 d3.show()
-d3.set_node_properties(label=node_name, color=node_color, cmap='Set1')
+
+d3.set_node_properties(label=df['label'].values, color=df['label'].values, cmap='Set1')
 d3.show()
 
 
 # %%
-from d3graph import d3graph, vec2adjmat
-
-source = ['node A', 'node F', 'node B', 'node B', 'node B', 'node A', 'node C', 'node Z']
-target = ['node F', 'node B', 'node J', 'node F', 'node F', 'node M', 'node M', 'node A']
-weight = [5.56, 0.5, 0.64, 0.23, 0.9, 3.28, 0.5, 0.45]
-
-# Convert to adjacency matrix
-adjmat = vec2adjmat(source, target, weight=weight)
-# print(adjmat)
+from d3graph import d3graph
+size = [10, 20, 10, 10, 15, 10, 5]
 
 # Example A: simple interactive network
 d3 = d3graph()
+
+# Load example
+adjmat = d3.import_example('bigbang')
+
 d3.graph(adjmat)
-d3.show()
-
-
-d3.node_properties['node_A']['size']=20
-d3.node_properties['node_A']['color']='#FF00FF'
 d3.show()
 
 # Example B: Color nodes
-# d3 = d3graph()
-d3.graph(adjmat)
-# Set node properties
 d3.set_node_properties(color=adjmat.columns.values)
 d3.show()
-
-
-size = [10, 20, 10, 10, 15, 10, 5]
 
 # Example C: include node size
 d3.set_node_properties(color=adjmat.columns.values, size=size)
@@ -91,7 +60,7 @@ d3.set_node_properties(color=adjmat.columns.values, size=size, edge_size=size[::
 d3.show()
 
 # Example E: include node-edge color
-d3.set_node_properties(color=adjmat.columns.values, size=size, edge_size=size[::-1], edge_color='#000000')
+d3.set_node_properties(color=adjmat.columns.values, size=size, edge_size=size[::-1], edge_color='#00FFFF')
 d3.show()
 
 # Example F: Change colormap
@@ -101,62 +70,41 @@ d3.show()
 # Example H: Include directed links. Arrows are set from source -> target
 d3.set_edge_properties(directed=True)
 d3.set_node_properties(color=adjmat.columns.values, size=size, edge_size=size, edge_color='#000FFF', cmap='Set1')
-d3.show(filepath='D://REPOS//erdogant.github.io//docs//d3graph//d3graph//example_2.html')
+d3.show()
 
 
 # %%
 from d3graph import d3graph, vec2adjmat
 d3 = d3graph()
+# Load example
+adjmat = d3.import_example('small')
 
-source = ['node A','node F','node B','node B','node B','node A','node C','node Z']
-target = ['node F','node B','node J','node F','node F','node M','node M','node A']
-weight = [5.56, 0.5, 0.64, 0.23, 0.9, 3.28, 0.5, 0.45]
-
-adjmat = vec2adjmat(source, target, weight=weight)
 d3.graph(adjmat)
 d3.set_node_properties(color=adjmat.columns.values)
 d3.show()
 
 # %% Extended example
-import networkx as nx
-import pandas as pd
 from d3graph import d3graph
 
-G = nx.karate_club_graph()
-adjmat = nx.adjacency_matrix(G).todense()
-adjmat=pd.DataFrame(index=range(0,adjmat.shape[0]), data=adjmat, columns=range(0,adjmat.shape[0]))
-adjmat.columns=adjmat.columns.astype(str)
-adjmat.index=adjmat.index.astype(str)
-adjmat.iloc[3,4]=5
-adjmat.iloc[4,5]=6
-adjmat.iloc[5,6]=7
-
-from tabulate import tabulate
-print(tabulate(adjmat.head(), tablefmt="grid", headers="keys"))
-
-df = pd.DataFrame(index=adjmat.index)
-df['degree']=np.array([*G.degree()])[:,1]
-df['other info']=np.array([*G.degree()])[:,1]
-node_size=df.degree.values*2
-node_color=[]
-for i in range(0,len(G.nodes)):
-    node_color.append(G.nodes[i]['club'])
-    node_name=node_color
-
-# Make some graphs
+# Initialize
 d3 = d3graph()
+# Load karate example
+adjmat, df = d3.import_example('karate')
+
+label = df['label'].values
+node_size = df['degree'].values
 
 d3.graph(adjmat)
-d3.set_node_properties(color=node_color, cmap='Set1')
+d3.set_node_properties(color=df['label'].values)
 d3.show()
 
-d3.set_node_properties(label=node_name, color=node_color, cmap='Set1')
+d3.set_node_properties(label=label, color=label, cmap='Set1')
 d3.show()
 
-d3.set_node_properties(adjmat, size=node_size)
+d3.set_node_properties(size=node_size)
 d3.show()
 
-d3.set_node_properties(color=node_size, size=node_size)
+d3.set_node_properties(color=label, size=node_size)
 d3.show()
 
 d3.set_edge_properties(edge_distance=100)
@@ -170,13 +118,46 @@ d3.show()
 
 d3 = d3graph(collision=1, charge=250)
 d3.graph(adjmat)
-d3.set_node_properties(color=node_name, size=node_size, edge_size=node_size, cmap='Set1')
+d3.set_node_properties(color=label, size=node_size, edge_size=node_size, cmap='Set1')
 d3.show()
 
 d3 = d3graph(collision=1, charge=250)
 d3.graph(adjmat)
-d3.set_node_properties(color=node_name, size=node_size, edge_size=node_size, edge_color='#00FFFF', cmap='Set1')
+d3.set_node_properties(color=label, size=node_size, edge_size=node_size, edge_color='#00FFFF', cmap='Set1')
 d3.show()
 
 
-# %%
+# %% default example
+from d3graph import d3graph
+
+# Initialize
+d3 = d3graph()
+
+# Load karate example
+adjmat, df = d3.import_example('karate')
+
+# Initialize
+d3.graph(adjmat)
+
+# Node properties
+d3.set_node_properties(label=df['label'].values, color=df['label'].values, size=df['degree'].values, edge_size=df['degree'].values, cmap='Set1')
+
+# Plot
+d3.show()
+
+
+# %% Collision example
+from d3graph import d3graph
+
+# Initialize
+d3 = d3graph(charge=1000)
+
+# Load karate example
+adjmat, df = d3.import_example('karate')
+
+# Initialize
+d3.graph(adjmat)
+
+# Plot
+d3.show(filepath='D://REPOS//erdogant.github.io//docs//d3graph//d3graph//karate_charge.html', figsize=(800, 600))
+
