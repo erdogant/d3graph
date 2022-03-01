0bf6254729017e6624c2856cffa44ce6224a9ac6
erdogant <erdogant@gmail.com>
2022-03-01 21:11:55 +0100

    experiment with css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,10 +84,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = ['css/custom.css',]
 
 # A list of files that should not be packed into the epub file
 epub_exclude_files = ['search.html']
 
 html_sidebars = {'**': ['localtoc.html', 'carbonads.html', 'relations.html', 'sourcelink.html', 'searchbox.html',],}
-
-html_css_files = ['custom.css']
