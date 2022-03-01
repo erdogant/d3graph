59ff7e77b1d5ebcbd43079e79c74e61436de3071
erdogant <erdogant@gmail.com>
2022-03-01 20:20:36 +0100

    carbon ad experiment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,3 +83,13 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+## In conf.py
+html_sidebars = { '**': [
+    'localtoc.html',
+    'carbonads.html',  # Put the ad below the navigation but above previous/next
+    'relations.html',
+    'sourcelink.html',
+    'searchbox.html',
+] }
