735bc8660b7d41c12642ae3039d9d411e6e964b1
rjhear <72757968+rjhear@users.noreply.github.com>
2022-08-05 21:53:11 -0400

    Format, spell check

--- setup.py
+++ setup.py
@@ -1,35 +1,24 @@
-import setuptools
 import re
 
-# versioning ------------
-VERSIONFILE="d3graph/__init__.py"
-getversion = re.search( r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M)
-if getversion:
-    new_version = getversion.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+import setuptools
 
-# Setup ------------
+# Versioning -----------------------------------------------------------------------------------------------------------
+VERSIONFILE = "d3graph/__init__.py"
+if getversion := re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M):
+    new_version = getversion[1]
+else:
+    raise RuntimeError(f"Unable to find version string in {VERSIONFILE}.")
+
+# Setup ----------------------------------------------------------------------------------------------------------------
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     install_requires=['pandas','numpy','colourmap','networkx>2','ismember','jinja2', 'sklearn', 'packaging', 'markupsafe==2.0.1', 'python-louvain'],
-     python_requires='>=3',
-     name='d3graph',
-     version=new_version,
-     author="Erdogan Taskesen",
-     author_email="erdogant@gmail.com",
-     description="Python package to create interactive network based on d3js.",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://erdogant.github.io/d3graph",
-	 download_url = 'https://github.com/erdogant/d3graph/archive/'+new_version+'.tar.gz',
-     packages=setuptools.find_packages(), # Searches throughout all dirs for files to include
-     include_package_data=True, # Must be true to include files depicted in MANIFEST.in
-     license_files=["LICENSE"],
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: BSD License",
-         "Operating System :: OS Independent",
-     ],
- )
+    install_requires=['pandas', 'numpy', 'colourmap', 'networkx>2', 'ismember', 'jinja2', 'sklearn', 'packaging',
+                      'markupsafe==2.0.1', 'python-louvain'], python_requires='>=3', name='d3graph',
+    version=new_version, author="Erdogan Taskesen", author_email="erdogant@gmail.com",
+    description="Python package to create interactive network based on d3js.", long_description=long_description,
+    long_description_content_type="text/markdown", url="https://erdogant.github.io/d3graph",
+    download_url=f'https://github.com/erdogant/d3graph/archive/{new_version}.tar.gz',
+    packages=setuptools.find_packages(), include_package_data=True, license_files=["LICENSE"],
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: BSD License",
+                 "Operating System :: OS Independent"])
