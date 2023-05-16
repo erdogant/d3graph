16e55f075616e28e0fa75a32f62ff8468c8e0621
erdogant <erdogant@gmail.com>
2023-05-17 00:09:58 +0200

    add font monospace as fonttype

--- d3graph/d3js/d3graphscript.js
+++ d3graph/d3js/d3graphscript.js
@@ -115,8 +115,9 @@ function d3graphscript(config = {
     .attr("dy", ".35em")
     .text(function(d) {return d.node_name}) // NODE-TEXT
     .style("font-size", function(d) {return d.node_fontsize + "px";}) // set font size equal to node edge size
-	.style("fill", function(d) {return d.node_fontcolor;}); // set the text fill color to the same as node color
-  //  .style("stroke", "gray");
+	.style("fill", function(d) {return d.node_fontcolor;}) // set the text fill color to the same as node color
+	.style("font-family", "monospace");
+//  .style("stroke", "gray");
 
   let showInHover = ["node_tooltip"]; // Tooltip
   node.append("title")
