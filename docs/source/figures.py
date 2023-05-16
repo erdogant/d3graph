3d62ddda6ab0ea45456cdd15a28eab56e2e14ce3
erdogant <erdogant@gmail.com>
2023-05-16 19:53:08 +0200

    2.4.5

--- docs/source/figures.py
+++ docs/source/figures.py
@@ -103,3 +103,46 @@ d3.set_node_properties(label=label, tooltip=tooltip, color=label, minmax=[0.1, 2
 d3.show(figsize=(500, 600), filepath=r'D:\REPOS\erdogant.github.io\docs\d3blocks\karate_label_hover.html', overwrite=True)
 
 # %%
+from d3graph import d3graph
+
+# Initialize
+d3 = d3graph()
+# Load karate example
+adjmat, df = d3.import_example('karate')
+
+label = df['label'].values
+node_size = df['degree'].values
+
+d3.graph(adjmat)
+d3.set_node_properties(color=df['label'].values)
+d3.show()
+
+d3.set_node_properties(label=label, color=label, cmap='Set1')
+d3.show()
+
+d3.set_node_properties(size=node_size)
+d3.show()
+
+d3.set_node_properties(color=label, size=node_size)
+d3.show()
+
+d3.set_edge_properties(edge_distance=100)
+d3.set_node_properties(color=node_size, size=node_size)
+d3.show()
+
+d3 = d3graph(charge=1000)
+d3.graph(adjmat)
+d3.set_node_properties(color=node_size, size=node_size)
+d3.show()
+
+d3 = d3graph(collision=1, charge=250)
+d3.graph(adjmat)
+d3.set_node_properties(color=label, size=node_size, edge_size=node_size, cmap='Set1')
+d3.show()
+
+d3 = d3graph(collision=1, charge=250)
+d3.graph(adjmat)
+d3.set_node_properties(color=label, size=node_size, edge_size=node_size, edge_color='#00FFFF', cmap='Set1')
+d3.show()
+
+# %%
