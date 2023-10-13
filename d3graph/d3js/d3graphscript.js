fa5857b0cb0e29667018a737d1bc3be5b6699ff7
erdogant <erdogant@gmail.com>
2023-10-13 21:58:53 +0200

    add zoom behavior back. For some reason this was commented out.

--- d3graph/d3js/d3graphscript.js
+++ d3graph/d3js/d3graphscript.js
@@ -48,8 +48,8 @@ function d3graphscript(config = {
   var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
-//    .call(d3.behavior.zoom().on("zoom", function () { svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")") }))
-//    .on("dblclick.zoom", null)
+    .call(d3.behavior.zoom().on("zoom", function () { svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")") }))
+    .on("dblclick.zoom", null)
     .append("g")
 
   //.on("dblclick", threshold); // EXPLODE ALL CONNECTED POINTS
