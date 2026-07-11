e8055475487a71aaff6980b60dd36c46d79ead08
erdogant <erdogant@gmail.com>
2026-07-11 13:44:37 +0200

    2.9.1

--- d3graph/__init__.py
+++ d3graph/__init__.py
@@ -17,7 +17,7 @@ from d3graph.d3graph import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '2.9.0'
+__version__ = '2.9.1'
 
 # Setup root logger
 _logger = logging.getLogger('d3graph')
